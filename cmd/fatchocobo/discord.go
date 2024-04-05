b00e94fda7357b76deb305b8050eab13f3f75c08
jluong102 <jluong102@gmail.com>
2024-04-05 10:35:51 -0400

    Added gateway http request

--- cmd/fatchocobo/discord.go
+++ cmd/fatchocobo/discord.go
@@ -1,9 +1,36 @@
 package main
 
+import (
+	"net/http"
+	"fmt"
+)
+
 const DISCORD_URL string = "https://discord.com/api"
 
 type Discord struct {
 	token string
+}
+
+// HTTP Requests
+func (this Discord) GetGatewayBot() (*http.Response, error) {
+	endpoint := DISCORD_URL + "/gateway/bot"
+
+	request, err := http.NewRequest(http.MethodGet, endpoint, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return this.MakeRequest(request)
+}
+
+// General HTTP request for discord]
+func (this Discord) MakeRequest(request *http.Request) (*http.Response, error) {
+	request.Header["Authorization"] = []string{fmt.Sprintf("Bot %s", this.token)}
+	request.Header[""] = []string{"DiscordBot (Fat Chocobo, 0)"}
+
+	client := http.Client{}
+	return client.Do(request)
 }
 
 // Constructor 
