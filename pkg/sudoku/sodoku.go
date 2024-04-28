898cbaedb27efde8d1b31d2e23df464e24369fd3
jluong102 <jluong102@gmail.com>
2024-04-27 20:40:20 -0400

    Added parse

--- pkg/sudoku/sodoku.go
+++ pkg/sudoku/sodoku.go
@@ -3,6 +3,7 @@ package sudoku
 import (
 	"encoding/json"
 	"net/http"
+	"io"
 )
 
 const SUDOKU_URL string = "https://sudoku-api.vercel.app/api/dosuku"
@@ -19,11 +20,24 @@ type Board struct {
 	} `json:"newboard"`
 }
 
-func GetBoard() (*http.Response, error){
+func GetBoard() (*http.Response, error) {
 	request := http.NewRequest(http.MethodGet, SUDOKU_URL, nil)
 
 	client := http.Client{}
 	return client.Do(request)
 }
 
+func ParseBoardResponse(request *http.Response) (*board, error) {
+	board := new(Board)
 
+	raw, err := io.ReadAll(request.Body)
+	defer request.Body.Close()	
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(raw, board)
+
+	return board, err	
+}
